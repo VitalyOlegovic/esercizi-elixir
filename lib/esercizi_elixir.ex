31f83d43964f13e9f34182a41603758373bed07b
Vitalij Zadneprovskij <vitalij_zad@libero.it>
2020-04-08 21:32:10 +0200

    Let's review

--- lib/esercizi_elixir.ex
+++ lib/esercizi_elixir.ex
@@ -19,25 +19,6 @@ defmodule EserciziElixir do
     Enum.map(1..10, fn y -> "#{n} x #{y} = #{y*n}" end) |> Enum.each(fn x -> IO.puts(x) end)
   end
 
-  def inner_lets_review([a|[b|c]], odd, even) do
-    inner_lets_review(c, [a|odd], [b|even])
-  end
-
-  def inner_lets_review([a|_], odd, even) do
-    {[a|odd],even}
-  end
-
-  def inner_lets_review([], odd, even) do
-    {odd,even}
-  end
-
-  def lets_review(string) do
-    {odd,even} = inner_lets_review(string,[],[])
-    o=odd |> Enum.reverse |> List.to_string
-    e=even |> Enum.reverse |> List.to_string
-    o <> " " <> e
-  end
-
   def hello do
     :world
   end
---
+++ lib/lets_review.ex
@@ -0,0 +1,29 @@
+defmodule LetsReview do
+
+  def inner_lets_review([a|[b|c]], odd, even) do
+    inner_lets_review(c, [a|odd], [b|even])
+  end
+
+  def inner_lets_review([a|_], odd, even) do
+    {[a|odd],even}
+  end
+
+  def inner_lets_review([], odd, even) do
+    {odd,even}
+  end
+
+  def lets_review(string) do
+    {odd,even} = inner_lets_review(string,[],[])
+    o=odd |> Enum.reverse |> List.to_string
+    e=even |> Enum.reverse |> List.to_string
+    o <> " " <> e
+  end
+
+  def read_line do
+    IO.gets("") |> String.trim |> String.to_charlist |> lets_review |> IO.puts
+  end
+
+end
+
+line_number = IO.gets("") |> String.trim |> Integer.parse |> elem(0)
+1..line_number |> Enum.each(fn _ -> LetsReview.read_line end)
